1dcd25626e93abe2e2d48c6bdef18352c67a23df
dattebayoRob <robson.william65@gmail.com>
2019-06-11 23:41:00 -0300

    Backend: Resize images uploaded and event emmiter with socket.io

--- backend/src/controller/PostController.js
+++ backend/src/controller/PostController.js
@@ -1,4 +1,7 @@
 const Post = require('../model/Post')
+const sharp = require('sharp')
+const path = require('path')
+const fs = require('fs')
 
 module.exports = {
     async index(req, res) {
@@ -10,9 +13,21 @@ module.exports = {
         const { author, place, description, hashtags } = req.body
         const { filename: image } = req.file
 
+        const [ name ] = image.split('.')
+        const fileName = `${name}.jpg`
+
+        await sharp(req.file.path)
+                .resize(500)
+                .jpeg({quality: 70})
+                .toFile(path.resolve(req.file.destination, 'resized', fileName))
+
+        fs.unlinkSync(req.file.path)
+
         const post = await Post.create({
-            author,place,description,hashtags,image
+            author,place,description,hashtags,image: fileName
         })
+
+        req.io.emit('post', post)
 
         return res.json(post)
     }
